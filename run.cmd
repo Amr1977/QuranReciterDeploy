9d2c1f4d971611745ed937964ef6fae10d352f21
amr lotfy <amr.lotfy@badrit.com>
2015-04-09 12:07:19 +0200

    fixed file separator.

--- run.cmd
+++ run.cmd
@@ -1,4 +1,15 @@
 @echo off
-set path=%~dp0\libs;%path%
-java -jar quranreciter.jar
+rem extract java runtime engine files if needed
+IF EXIST %~dp0\JRE8\NUL GOTO SKIP_EXTRACT_JRE
+echo Extracting JAVA JRE ...
+%~dp0\JRE8.EXE
+:SKIP_EXTRACT_JRE
 
+rem extract library files 
+IF EXIST %~dp0\lib\NUL GOTO SKIP_EXTRACT_LIB
+echo Extracting application Libraries ...
+%~dp0\LIB.EXE
+:SKIP_EXTRACT_LIB
+set path=%~dp0;%~dp0\lib;%~dp0\jre8\bin;%~dp0\jre8\lib;%~dp0\jre8;%path%
+java -jar quranreciter.jar
+pause
